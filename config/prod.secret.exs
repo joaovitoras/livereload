ed402710b451e9a1124ed504b9fb2ba8f25867b3
João Vitor Alves <joao.vitor@redealumni.com>
2018-08-06 23:32:34 -0300

    adds secret

---
+++ config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :livereload, LivereloadWeb.Endpoint,
+  secret_key_base: "ilfSdV6XYVvnYs6ZtjuEeHOTzec6NqHdX3u6ZJPjY10qM0jtmAGzaxM2P3iOnqWT"
